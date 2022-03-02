e4ea09df1f56ff4fd8fb66754450613a737db00e
Mendel Jacks <mendeljacks@gmail.com>
2022-03-02 01:55:25 -0500

    push

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env -S node -r "sucrase/register"
-// require('sucrase/register/ts-legacy-module-interop')
-// require('sucrase/register/ts')
+#!/usr/bin/env sucrase-node
+require('sucrase/register/ts')
 require('./src/index')
